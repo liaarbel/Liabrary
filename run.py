1a3e2e3e82c334be3d0bf360c420c985c4825ac3
liaarbel <lia.arbel1@gmail.com>
2021-01-08 16:16:25 +0200

    added run.py

--- run.py
+++ run.py
@@ -8,22 +8,31 @@ from flask_peewee.db import Database
 from app.models import all_models
 from app.views import views_app
 
+LOCAL = False
 
 if __name__ == '__main__':
-    url = parse.urlparse(os.environ['DATABASE_URL'])
-    DATABASE = {
-        'engine': 'peewee.PostgresqlDatabase',
-        'name': url.path[1:],
-        'user': url.username,
-        'password': url.password,
-        'host': url.hostname,
-        'port': url.port,
-    }
+    if LOCAL:
+        DATABASE = {
+            'name': 'books2.db',
+            'engine': 'peewee.SqliteDatabase',
+        }
+    else:
+        url = parse.urlparse(os.environ['DATABASE_URL'])
+        DATABASE = {
+            'engine': 'peewee.PostgresqlDatabase',
+            'name': url.path[1:],
+            'user': url.username,
+            'password': url.password,
+            'host': url.hostname,
+            'port': url.port,
+        }
 
-    app = Flask(__name__, static_folder=str((Path(__file__).parent / 'static').absolute()))
+    app = Flask(__name__,
+                static_folder=str((Path(__file__).parent / 'app' / 'static').absolute()),
+                template_folder=str((Path(__file__).parent / 'app' / 'templates').absolute()))
     app.config.from_object(__name__)
     app.register_blueprint(views_app)
     db = Database(app)
-    db.bind(all_models)
+    db.database.bind(all_models)
     db.database.create_tables(all_models, fail_silently=True)
     app.run(threaded=True, port=5000)
